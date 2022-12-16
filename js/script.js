3770f40ce9cbc52fbd100d21440ecfc866bc002e
fedeloxe <loxeee@gmail.com>
2022-12-16 15:36:52 +0100

    img and text added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const{
+    createApp
+}=Vue;
+
+createApp({
+    data(){
+        return{
+            name:'',
+            surname:'',
+            url:'https://www.licondividi.it/wp-content/uploads/2021/10/immagini-buon-weekend-83.jpg'
+        }
+    }
+}).mount('#app')
